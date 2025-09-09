a9745ea9383be89c088e9bdb2de2fe43c78e42fb
LokeshVerma54001 <148640056+LokeshVerma54001@users.noreply.github.com>
2025-09-09 12:37:08 +0530

    minor db fixes

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -6,17 +6,17 @@ import { User } from "../models/user.model.js";
 export const inngest = new Inngest({ id: "video-app" });
 
 const syncUser = inngest.createFunction(
-    {id: "sync-user"},
-    {event: "clerk/user.created"},
-    async ({event})=>{
+    { id: "sync-user" },
+    { event: "clerk/user.created" },
+    async ({ event }) => {
         await connectDB();
-        const {id, email_addresses, first_name, last_name, image_url} =  event.data;
+        const { id, email_addresses, first_name, last_name, image_url } = event.data;
         const newUser = {
-            clerkId:id, 
-            email: email_addresses[0]?.email_addresses,
-            name: `${first_name || ""} ${last_name || ""}`,
-            image: image_url,
-        }
+        clerkId: id,
+        email: email_addresses[0]?.email_address,
+        name: `${first_name || ""} ${last_name || ""}`,
+        image: image_url,
+        };
         await User.create(newUser);
     }
 )
